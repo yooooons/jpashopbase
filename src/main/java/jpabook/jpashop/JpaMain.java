61d1239e2f53daa7d6ec24fb6811cca1f585d59c
unknown <yooooonnf@gmail.com>
2022-04-09 03:00:41 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+
+        try {
+            tx.commit();
+
+        } catch (Exception e) {
+            tx.rollback();
+        }finally {
+            em.close();
+
+        }
+
+        emf.close();
+
+
+
+
+    }
+}
